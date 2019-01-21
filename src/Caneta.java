8be64837f008bafd3abe905500a82ab76cc201e4
Thiago Trabalho <thiagodasilvamaduro@gmail.com>
2019-01-21 12:00:14 -0400

    criacao do projeto

---
+++ src/Caneta.java
@@ -0,0 +1,27 @@
+public class Caneta {
+
+	String modelo;
+	String cor;
+	float ponta;
+	int carga;
+	boolean tampada;
+
+	// void significa "sem retorno"
+	void status() {
+		
+		
+	}
+	
+	void rabiscar() {
+
+	}
+
+	void tampar() {
+
+	}
+
+	void destampar() {
+
+	}
+
+}
